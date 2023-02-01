ea6bad4e9422ca4daa0f615367d702938e22a531
Glitched Polygons <glitchedpolygons@protonmail.com>
2023-02-01 15:11:59 +0100

    Initial commit.

---
+++ DirSyncSFTP/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirSyncSFTP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using NotifyIcon notifyIcon = new();
+
+            notifyIcon.Icon = new System.Drawing.Icon("sftp.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick +=
+                delegate (object? sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
+    }
+}
